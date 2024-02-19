8020118f0d135476e908ece4e2a12e0b645c3a5a
KevinLo <16764256+kevinlobcit@users.noreply.github.com>
2024-02-19 04:39:32 -0800

    added character replace

--- program.py
+++ program.py
@@ -5,13 +5,28 @@ def loadworddic(dicarray):
     f = open("words_alpha.txt", "r")
     for word in f:
         dicarray.append(word.strip())
+        
+#randomly picks a word from dictionary array
+def pickword(dicarray):
+    pick = round(random.random() * len(dictionary))
+    return dicarray[pick]
+
+#replaces a character in cesnored
+def spotreplace(letter, censored, index):
+    return censored[:index] + letter + censored[index+1:]
+    
 
 #save the words in an array 
 dictionary = []
 loadworddic(dictionary)
 
 #pick a word from the array
-pick = round(random.random() * len(dictionary))
-#print(dictionary)
+word = pickword(dictionary)
+word = "abcdefg"
 
-print(pick) 
+censored = '_'*len(word)
+
+print(word)
+print(censored)
+
+print(spotreplace("x", censored, 1))
